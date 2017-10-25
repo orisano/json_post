ce5b157fb54f9a9b8e8e374f434a0fc514fc5bba
Nao YONASHIRO <yonashiro@r.recruit.co.jp>
2017-10-25 14:46:54 +0900

    feat: improve benchmark

--- request.go
+++ request.go
@@ -25,61 +25,33 @@ func RequestMarshal(rawurl string, data interface{}) error {
 	return nil
 }
 
-func RequestEncodeDefaultBuffer(rawurl string, data interface{}) error {
-	var b bytes.Buffer
-	if err := json.NewEncoder(&b).Encode(data); err != nil {
-		return err
-	}
-	req, err := http.NewRequest(http.MethodPost, rawurl, &b)
-	if err != nil {
-		return err
-	}
-	resp, err := http.DefaultClient.Do(req)
-	if err != nil {
-		return err
-	}
-	defer resp.Body.Close()
-	return nil
-}
-
-func RequestEncodeNewBuffer(rawurl string, data interface{}) error {
-	b := new(bytes.Buffer)
-	if err := json.NewEncoder(b).Encode(data); err != nil {
-		return err
-	}
-	req, err := http.NewRequest(http.MethodPost, rawurl, b)
-	if err != nil {
-		return err
-	}
-	resp, err := http.DefaultClient.Do(req)
-	if err != nil {
-		return err
-	}
-	defer resp.Body.Close()
-	return nil
-}
-
-func RequestEncodeNewNilBuffer(rawurl string, data interface{}) error {
-	b := bytes.NewBuffer(nil)
-	if err := json.NewEncoder(b).Encode(data); err != nil {
-		return err
-	}
-	req, err := http.NewRequest(http.MethodPost, rawurl, b)
-	if err != nil {
-		return err
-	}
-	resp, err := http.DefaultClient.Do(req)
-	if err != nil {
-		return err
-	}
-	defer resp.Body.Close()
-	return nil
-}
-
-func RequestEncodeReservedBuffer(sizeHint int) func(string, interface{}) error {
+func RequestEncodeDefaultBuffer(escape bool) func(rawurl string, data interface{}) error {
 	return func(rawurl string, data interface{}) error {
-		b := bytes.NewBuffer(make([]byte, 0, sizeHint))
-		if err := json.NewEncoder(b).Encode(data); err != nil {
+		var b bytes.Buffer
+		enc := json.NewEncoder(&b)
+		enc.SetEscapeHTML(escape)
+		if err := enc.Encode(data); err != nil {
+			return err
+		}
+		req, err := http.NewRequest(http.MethodPost, rawurl, &b)
+		if err != nil {
+			return err
+		}
+		resp, err := http.DefaultClient.Do(req)
+		if err != nil {
+			return err
+		}
+		defer resp.Body.Close()
+		return nil
+	}
+}
+
+func RequestEncodeNewBuffer(escape bool) func(rawurl string, data interface{}) error {
+	return func(rawurl string, data interface{}) error {
+		b := new(bytes.Buffer)
+		enc := json.NewEncoder(b)
+		enc.SetEscapeHTML(escape)
+		if err := enc.Encode(data); err != nil {
 			return err
 		}
 		req, err := http.NewRequest(http.MethodPost, rawurl, b)
@@ -95,19 +67,66 @@ func RequestEncodeReservedBuffer(sizeHint int) func(string, interface{}) error {
 	}
 }
 
-func RequestEncodePipe(rawurl string, data interface{}) error {
-	pr, pw := io.Pipe()
-	go func() {
-		pw.CloseWithError(json.NewEncoder(pw).Encode(data))
-	}()
-	req, err := http.NewRequest(http.MethodPost, rawurl, pr)
-	if err != nil {
-		return err
+func RequestEncodeNewNilBuffer(escape bool) func(rawurl string, data interface{}) error {
+	return func(rawurl string, data interface{}) error {
+		b := bytes.NewBuffer(nil)
+		enc := json.NewEncoder(b)
+		enc.SetEscapeHTML(escape)
+		if err := enc.Encode(data); err != nil {
+			return err
+		}
+		req, err := http.NewRequest(http.MethodPost, rawurl, b)
+		if err != nil {
+			return err
+		}
+		resp, err := http.DefaultClient.Do(req)
+		if err != nil {
+			return err
+		}
+		defer resp.Body.Close()
+		return nil
 	}
-	resp, err := http.DefaultClient.Do(req)
-	if err != nil {
-		return err
+}
+
+func RequestEncodeReservedBuffer(sizeHint int, escape bool) func(string, interface{}) error {
+	return func(rawurl string, data interface{}) error {
+		b := bytes.NewBuffer(make([]byte, 0, sizeHint))
+		enc := json.NewEncoder(b)
+		enc.SetEscapeHTML(escape)
+		if err := enc.Encode(data); err != nil {
+			return err
+		}
+		req, err := http.NewRequest(http.MethodPost, rawurl, b)
+		if err != nil {
+			return err
+		}
+		resp, err := http.DefaultClient.Do(req)
+		if err != nil {
+			return err
+		}
+		defer resp.Body.Close()
+		return nil
 	}
-	defer resp.Body.Close()
-	return nil
+}
+
+func RequestEncodePipe(escape bool) func(rawurl string, data interface{}) error {
+	return func(rawurl string, data interface{}) error {
+		pr, pw := io.Pipe()
+		go func() {
+			enc := json.NewEncoder(pw)
+			enc.SetEscapeHTML(escape)
+			pw.CloseWithError(enc.Encode(data))
+		}()
+		req, err := http.NewRequest(http.MethodPost, rawurl, pr)
+		if err != nil {
+			return err
+		}
+		resp, err := http.DefaultClient.Do(req)
+		if err != nil {
+			return err
+		}
+		defer resp.Body.Close()
+		return nil
+	}
+
 }
--- request_test.go
+++ request_test.go
@@ -5,6 +5,7 @@ import (
 	"net/http/httptest"
 	"strings"
 	"testing"
+	"time"
 )
 
 type yesman struct{}
@@ -27,6 +28,25 @@ var largeObject map[string]interface{} = map[string]interface{}{
 	"text": strings.Repeat("1", 30*1024*1024),
 }
 
+type simpleStruct struct {
+	ID       string  `json:"id"`
+	Password string  `json:"password"`
+	Email    string  `json:"email"`
+	Age      int     `json:"age"`
+	Weight   float64 `json:"weight"`
+}
+
+type Location struct {
+	Latitude  float64 `json:"latitude"`
+	Longitude float64 `json:"longitude"`
+}
+
+type nestedStruct struct {
+	Locations []Location `json:"locations"`
+	Name      string     `json:"name"`
+	StartTime time.Time  `json:"start_time"`
+}
+
 func benchmarkRequest(b *testing.B, name string, fn func(string, interface{}) error, rawurl string, data interface{}) {
 	b.Run(name, func(b *testing.B) {
 		b.ResetTimer()
@@ -44,37 +64,111 @@ func BenchmarkSmall(b *testing.B) {
 	s := httptest.NewServer(&yesman{})
 	defer s.Close()
 	o := smallObject
-	benchmarkRequest(b, "RequestMarshal              ", RequestMarshal, s.URL, o)
-	benchmarkRequest(b, "RequestEncodeDefaultBuffer  ", RequestEncodeDefaultBuffer, s.URL, o)
-	benchmarkRequest(b, "RequestEncodeNewBuffer      ", RequestEncodeNewBuffer, s.URL, o)
-	benchmarkRequest(b, "RequestEncodeNewNilBuffer   ", RequestEncodeNewNilBuffer, s.URL, o)
-	benchmarkRequest(b, "RequestEncodeReservedBuffer ", RequestEncodeReservedBuffer(1024), s.URL, o)
-	benchmarkRequest(b, "RequestEncodeCheatBuffer    ", RequestEncodeReservedBuffer(128), s.URL, o)
-	benchmarkRequest(b, "RequestEncodePipe           ", RequestEncodePipe, s.URL, o)
+	benchmarkRequest(b, "MarshalE              ", RequestMarshal, s.URL, o)
+	benchmarkRequest(b, "EncodeDefaultBuffer   ", RequestEncodeDefaultBuffer(false), s.URL, o)
+	benchmarkRequest(b, "EncodeDefaultBufferE  ", RequestEncodeDefaultBuffer(true), s.URL, o)
+	benchmarkRequest(b, "EncodeNewBuffer       ", RequestEncodeNewBuffer(false), s.URL, o)
+	benchmarkRequest(b, "EncodeNewBufferE      ", RequestEncodeNewBuffer(true), s.URL, o)
+	benchmarkRequest(b, "EncodeNewNilBuffer    ", RequestEncodeNewNilBuffer(false), s.URL, o)
+	benchmarkRequest(b, "EncodeNewNilBufferE   ", RequestEncodeNewNilBuffer(true), s.URL, o)
+	benchmarkRequest(b, "EncodeReservedBuffer  ", RequestEncodeReservedBuffer(1024, false), s.URL, o)
+	benchmarkRequest(b, "EncodeReservedBufferE ", RequestEncodeReservedBuffer(1024, true), s.URL, o)
+	benchmarkRequest(b, "EncodeCheatBuffer     ", RequestEncodeReservedBuffer(128, false), s.URL, o)
+	benchmarkRequest(b, "EncodeCheatBufferE    ", RequestEncodeReservedBuffer(128, true), s.URL, o)
+	benchmarkRequest(b, "EncodePipe            ", RequestEncodePipe(false), s.URL, o)
+	benchmarkRequest(b, "EncodePipeE           ", RequestEncodePipe(true), s.URL, o)
 }
 
 func BenchmarkMiddle(b *testing.B) {
 	s := httptest.NewServer(&yesman{})
 	defer s.Close()
 	o := middleObject
-	benchmarkRequest(b, "RequestMarshal             ", RequestMarshal, s.URL, o)
-	benchmarkRequest(b, "RequestEncodeDefaultBuffer ", RequestEncodeDefaultBuffer, s.URL, o)
-	benchmarkRequest(b, "RequestEncodeNewBuffer     ", RequestEncodeNewBuffer, s.URL, o)
-	benchmarkRequest(b, "RequestEncodeNewNilBuffer  ", RequestEncodeNewNilBuffer, s.URL, o)
-	benchmarkRequest(b, "RequestEncodeReservedBuffer", RequestEncodeReservedBuffer(50*1024), s.URL, o)
-	benchmarkRequest(b, "RequestEncodeCheatBuffer   ", RequestEncodeReservedBuffer(32*1024), s.URL, o)
-	benchmarkRequest(b, "RequestEncodePipe          ", RequestEncodePipe, s.URL, o)
+	benchmarkRequest(b, "MarshalE             ", RequestMarshal, s.URL, o)
+	benchmarkRequest(b, "EncodeDefaultBuffer  ", RequestEncodeDefaultBuffer(false), s.URL, o)
+	benchmarkRequest(b, "EncodeDefaultBufferE ", RequestEncodeDefaultBuffer(true), s.URL, o)
+	benchmarkRequest(b, "EncodeNewBuffer      ", RequestEncodeNewBuffer(false), s.URL, o)
+	benchmarkRequest(b, "EncodeNewBufferE     ", RequestEncodeNewBuffer(true), s.URL, o)
+	benchmarkRequest(b, "EncodeNewNilBuffer   ", RequestEncodeNewNilBuffer(false), s.URL, o)
+	benchmarkRequest(b, "EncodeNewNilBufferE  ", RequestEncodeNewNilBuffer(true), s.URL, o)
+	benchmarkRequest(b, "EncodeReservedBuffer ", RequestEncodeReservedBuffer(50*1024, false), s.URL, o)
+	benchmarkRequest(b, "EncodeReservedBufferE", RequestEncodeReservedBuffer(50*1024, true), s.URL, o)
+	benchmarkRequest(b, "EncodeCheatBuffer    ", RequestEncodeReservedBuffer(32*1024, false), s.URL, o)
+	benchmarkRequest(b, "EncodeCheatBufferE   ", RequestEncodeReservedBuffer(32*1024, true), s.URL, o)
+	benchmarkRequest(b, "EncodePipe           ", RequestEncodePipe(false), s.URL, o)
+	benchmarkRequest(b, "EncodePipeE          ", RequestEncodePipe(true), s.URL, o)
 }
 
 func BenchmarkLarge(b *testing.B) {
 	s := httptest.NewServer(&yesman{})
 	defer s.Close()
 	o := largeObject
-	benchmarkRequest(b, "RequestMarshal              ", RequestMarshal, s.URL, o)
-	benchmarkRequest(b, "RequestEncodeDefaultBuffer  ", RequestEncodeDefaultBuffer, s.URL, o)
-	benchmarkRequest(b, "RequestEncodeNewBuffer      ", RequestEncodeNewBuffer, s.URL, o)
-	benchmarkRequest(b, "RequestEncodeNewNilBuffer   ", RequestEncodeNewNilBuffer, s.URL, o)
-	benchmarkRequest(b, "RequestEncodeReservedBuffer ", RequestEncodeReservedBuffer(40*1024*1024), s.URL, o)
-	benchmarkRequest(b, "RequestEncodeCheatBuffer    ", RequestEncodeReservedBuffer(32*1024*1024), s.URL, o)
-	benchmarkRequest(b, "RequestEncodePipe           ", RequestEncodePipe, s.URL, o)
+	benchmarkRequest(b, "MarshalE              ", RequestMarshal, s.URL, o)
+	benchmarkRequest(b, "EncodeDefaultBuffer   ", RequestEncodeDefaultBuffer(false), s.URL, o)
+	benchmarkRequest(b, "EncodeDefaultBufferE  ", RequestEncodeDefaultBuffer(true), s.URL, o)
+	benchmarkRequest(b, "EncodeNewBuffer       ", RequestEncodeNewBuffer(false), s.URL, o)
+	benchmarkRequest(b, "EncodeNewBufferE      ", RequestEncodeNewBuffer(true), s.URL, o)
+	benchmarkRequest(b, "EncodeNewNilBuffer    ", RequestEncodeNewNilBuffer(false), s.URL, o)
+	benchmarkRequest(b, "EncodeNewNilBufferE   ", RequestEncodeNewNilBuffer(true), s.URL, o)
+	benchmarkRequest(b, "EncodeReservedBuffer  ", RequestEncodeReservedBuffer(40*1024*1024, false), s.URL, o)
+	benchmarkRequest(b, "EncodeReservedBufferE ", RequestEncodeReservedBuffer(40*1024*1024, true), s.URL, o)
+	benchmarkRequest(b, "EncodeCheatBuffer     ", RequestEncodeReservedBuffer(31*1024*1024, false), s.URL, o)
+	benchmarkRequest(b, "EncodeCheatBufferE    ", RequestEncodeReservedBuffer(31*1024*1024, true), s.URL, o)
+	benchmarkRequest(b, "EncodePipe            ", RequestEncodePipe(false), s.URL, o)
+	benchmarkRequest(b, "EncodePipeE           ", RequestEncodePipe(true), s.URL, o)
+}
+
+func BenchmarkSimple(b *testing.B) {
+	s := httptest.NewServer(&yesman{})
+	defer s.Close()
+	o := simpleStruct{
+		ID:       "test",
+		Password: "password",
+		Email:    "test@example.com",
+		Age:      18,
+		Weight:   45.0,
+	}
+	benchmarkRequest(b, "MarshalE             ", RequestMarshal, s.URL, o)
+	benchmarkRequest(b, "EncodeDefaultBuffer  ", RequestEncodeDefaultBuffer(false), s.URL, o)
+	benchmarkRequest(b, "EncodeDefaultBufferE ", RequestEncodeDefaultBuffer(true), s.URL, o)
+	benchmarkRequest(b, "EncodeNewBuffer      ", RequestEncodeNewBuffer(false), s.URL, o)
+	benchmarkRequest(b, "EncodeNewBufferE     ", RequestEncodeNewBuffer(true), s.URL, o)
+	benchmarkRequest(b, "EncodeNewNilBuffer   ", RequestEncodeNewNilBuffer(false), s.URL, o)
+	benchmarkRequest(b, "EncodeNewNilBufferE  ", RequestEncodeNewNilBuffer(true), s.URL, o)
+	benchmarkRequest(b, "EncodeReservedBuffer ", RequestEncodeReservedBuffer(1024, false), s.URL, o)
+	benchmarkRequest(b, "EncodeReservedBufferE", RequestEncodeReservedBuffer(1024, true), s.URL, o)
+	benchmarkRequest(b, "EncodeCheatBuffer    ", RequestEncodeReservedBuffer(128, false), s.URL, o)
+	benchmarkRequest(b, "EncodeCheatBufferE   ", RequestEncodeReservedBuffer(128, true), s.URL, o)
+	benchmarkRequest(b, "EncodePipe           ", RequestEncodePipe(false), s.URL, o)
+	benchmarkRequest(b, "EncodePipeE          ", RequestEncodePipe(true), s.URL, o)
+}
+
+func BenchmarkNested(b *testing.B) {
+	s := httptest.NewServer(&yesman{})
+	defer s.Close()
+	o := nestedStruct{
+		Locations: []Location{
+			{1, 2},
+			{3, 4},
+			{5, 6},
+			{7, 8},
+			{9, 10},
+			{11, 12},
+			{13, 14},
+		},
+		Name:      "Tokyo",
+		StartTime: time.Unix(10000000, 1000),
+	}
+	benchmarkRequest(b, "MarshalE             ", RequestMarshal, s.URL, o)
+	benchmarkRequest(b, "EncodeDefaultBuffer  ", RequestEncodeDefaultBuffer(false), s.URL, o)
+	benchmarkRequest(b, "EncodeDefaultBufferE ", RequestEncodeDefaultBuffer(true), s.URL, o)
+	benchmarkRequest(b, "EncodeNewBuffer      ", RequestEncodeNewBuffer(false), s.URL, o)
+	benchmarkRequest(b, "EncodeNewBufferE     ", RequestEncodeNewBuffer(true), s.URL, o)
+	benchmarkRequest(b, "EncodeNewNilBuffer   ", RequestEncodeNewNilBuffer(false), s.URL, o)
+	benchmarkRequest(b, "EncodeNewNilBufferE  ", RequestEncodeNewNilBuffer(true), s.URL, o)
+	benchmarkRequest(b, "EncodeReservedBuffer ", RequestEncodeReservedBuffer(1024, false), s.URL, o)
+	benchmarkRequest(b, "EncodeReservedBufferE", RequestEncodeReservedBuffer(1024, true), s.URL, o)
+	benchmarkRequest(b, "EncodeCheatBuffer    ", RequestEncodeReservedBuffer(256, false), s.URL, o)
+	benchmarkRequest(b, "EncodeCheatBufferE   ", RequestEncodeReservedBuffer(256, true), s.URL, o)
+	benchmarkRequest(b, "EncodePipe           ", RequestEncodePipe(false), s.URL, o)
+	benchmarkRequest(b, "EncodePipeE          ", RequestEncodePipe(true), s.URL, o)
 }
